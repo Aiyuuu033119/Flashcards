cbebd3d1095a9a35b86a3a12ffa37bbff07d711f
Ian Destura <idestura35.com>
2021-09-01 00:57:24 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+var contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+document.getElementById("save_card").addEventListener("click", () => {
+  addFlashcard();
+});
+
+document.getElementById("delete_cards").addEventListener("click", () => {
+  localStorage.clear();
+  flashcards.innerHTML = '';
+  contentArray = [];
+});
+
+document.getElementById("show_card_box").addEventListener("click", () => {
+  document.getElementById("modal").style.display = "flex";
+});
+
+document.getElementById("close_card_box").addEventListener("click", () => {
+  document.getElementById("modal").style.display = "none";
+});
+
+flashcardMaker = (text) => {
+  const flashcard = document.createElement("div");
+  const question = document.createElement('h2');
+  const answer = document.createElement('h2');
+
+  flashcard.className = 'flashcard';
+
+  // question.setAttribute("style", "border-top:1px solid red; padding: 15px; margin-top:30px");
+  question.textContent = text.my_question;
+
+  answer.setAttribute("style", "text-align:center; margin-top: 1rem; display:none; color: #8e81ff");
+  answer.textContent = text.my_answer;
+
+  flashcard.appendChild(question);
+  flashcard.appendChild(answer);
+
+  flashcard.addEventListener("click", () => {
+    if(answer.style.display == "none")
+      answer.style.display = "block";
+    else
+      answer.style.display = "none";
+  })
+
+  document.querySelector("#flashcards").appendChild(flashcard);
+}
+
+contentArray.forEach(flashcardMaker);
+
+addFlashcard = () => {
+  const question = document.querySelector("#question");
+  const answer = document.querySelector("#answer");
+
+  if(question.value==""&&answer.value==""){
+    return false;
+  }
+  let flashcard_info = {
+    'my_question' : question.value,
+    'my_answer'  : answer.value
+  }
+
+  contentArray.push(flashcard_info);
+  localStorage.setItem('items', JSON.stringify(contentArray));
+  flashcardMaker(contentArray[contentArray.length - 1]);
+  question.value = "";
+  answer.value = "";
+
+  document.getElementById("modal").style.display = "none";
+}
